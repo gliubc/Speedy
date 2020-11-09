e34401cf678be5d04758f748354499ea46595ced
baocheng <lbc_mail@163.com>
2020-11-09 13:40:01 +0800

    Swift

---
+++ Speedy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speedy
+//
+//  Created by Haier on 2020/11/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
